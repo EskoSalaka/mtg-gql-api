e45dfe62e8c6c61954265107f0c8192c4d9a43f2
Esko-Kalervo Salaka <esko.salaka@gmail.com>
2024-06-02 20:46:01 +0300

    Added a type for price info

---
+++ src/card/types/prices.type.ts
@@ -0,0 +1,36 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Expose } from 'class-transformer';
+
+@ObjectType({
+  description: `An object containing daily price information for this card, including usd, 
+  usd_foil, usd_etched, eur, eur_foil, eur_etched, and tix prices, as Decimals (strings). `,
+})
+export class Prices {
+  @Expose()
+  @Field({ nullable: true })
+  usd: string | null;
+
+  @Expose()
+  @Field({ nullable: true })
+  usd_foil: string | null;
+
+  @Expose()
+  @Field({ nullable: true })
+  usd_etched: string | null;
+
+  @Expose()
+  @Field({ nullable: true })
+  eur: string | null;
+
+  @Expose()
+  @Field({ nullable: true })
+  eur_foil: string | null;
+
+  @Expose()
+  @Field({ nullable: true })
+  eur_etched: string | null;
+
+  @Expose()
+  @Field({ nullable: true })
+  tix: string | null;
+}
