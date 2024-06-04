7ceb517037ef2aceaca95b89fbbcd5c3d8d53e45
Esko-Kalervo Salaka <esko.salaka@gmail.com>
2024-06-04 18:36:20 +0300

    fixed the global error filter to handle/catch gql/http errors correctly

--- src/modules/server-utils/filters/global-exception-filter.ts
+++ src/modules/server-utils/filters/global-exception-filter.ts
@@ -1,12 +1,32 @@
 import { Catch, HttpException, Logger, type ArgumentsHost } from '@nestjs/common';
-import type { GqlExceptionFilter } from '@nestjs/graphql';
+import { GqlArgumentsHost, type GqlExceptionFilter } from '@nestjs/graphql';
 
-@Catch(HttpException)
+@Catch()
 export class GlobalExceptionFilter implements GqlExceptionFilter {
   private logger = new Logger();
 
   catch(exception: HttpException, host: ArgumentsHost) {
-    this.logger.error(exception);
+    this.logger.error('excEEEEEption');
+
+    if (host.getType() === 'http') {
+      const ctx = host.switchToHttp();
+      const response = ctx.getResponse();
+      const request = ctx.getRequest();
+
+      const status = exception.getStatus();
+      const message = exception.getResponse();
+
+      response.status(status).json({
+        statusCode: status,
+        timestamp: new Date().toISOString(),
+        path: request.url,
+        message,
+      });
+    } else {
+      this.logger.error('GQL exception caught');
+      this.logger.error(exception);
+      return exception;
+    }
 
     return exception;
   }
